1cb4bf517899695b389bbf2467b604b6ba900687
OSSAONE\dhtkd <dhtkd4900@gmail.com>
2023-04-13 16:30:26 +0900

    게시물 등록 리스트구현

---
+++ src/main/java/com/kos/exam/boot/controller/UsrArticleController.java
@@ -0,0 +1,37 @@
+package com.kos.exam.boot.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kos.exam.boot.vo.Article;
+
+@Controller
+public class UsrArticleController {
+	int articlesLastId=0;
+	private List<Article> articles;
+	
+	public UsrArticleController() {
+		articles = new ArrayList<Article>();
+	}
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		int id=articlesLastId+1;
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		articlesLastId=id;
+		return article;
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public List<Article> getArticle() {
+		return articles;
+	}
+
+}
