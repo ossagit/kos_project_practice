7004db205f0c03aee25d2246d9a8fa987bcb5ad7
OSSAONE\dhtkd <dhtkd4900@gmail.com>
2023-04-28 14:27:04 +0900

    댓글삭제 활성화

--- src/main/java/com/kos/exam/boot/controller/UsrReplyController.java
+++ src/main/java/com/kos/exam/boot/controller/UsrReplyController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.kos.exam.boot.service.ReactionPointService;
 import com.kos.exam.boot.service.ReplyService;
 import com.kos.exam.boot.util.Ut;
+import com.kos.exam.boot.vo.Reply;
 import com.kos.exam.boot.vo.ResultData;
 import com.kos.exam.boot.vo.Rq;
 
@@ -46,6 +46,35 @@ public class UsrReplyController {
 			}
 		}
 		return rq.jsReplace(writeReplyRd.getMsg(), replaceUri);
+	}
+	
+	@RequestMapping("/usr/reply/doDelete")
+	@ResponseBody
+	String doDelete(int id, String replaceUri) {
+		if(Ut.empty(id)) {
+			return rq.jsHistoryBack("id를 입력해주세요.");
+		}
+		
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
+		
+		if(reply == null) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글이 존재하지 않습니다.", id));
+		}
+		
+		if(reply.isExtra_actorCanDelete()==false) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 삭제할 권한이 없습니다.", id));
+		}
+		
+		ResultData deleteReplyRd = replyService.deleteReply(id);
+		
+		if(Ut.empty(replaceUri)) {
+			switch(reply.getRelTypeCode()) {
+			case "article" :
+				replaceUri = Ut.f("../article/detail?id=%d", reply.getRelId());
+				break;
+			}
+		}
+		return rq.jsReplace(deleteReplyRd.getMsg(), replaceUri);
 	}
 
 }
